9ea263594cc7893c9d6c2ebc90390176f13e43a4
Tyler Confalone <tconfalone@gmail.com>
2018-08-13 18:16:33 -0700

    changed default page and erased old landing stuff

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,29 +1,21 @@
 import React, {Component, Fragment} from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-import Landing from '../landing';
 import Dashboard from '../dashboard';
 
 
 export default class App extends Component {
- render() {
-   return(
-    <Fragment>
-      <BrowserRouter>
+  render() {
+    return(
       <Fragment>
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/dashboard'>Dashboard</Link></li>
-          </ul>
-        </nav>
-        <div>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/dashboard' component={Dashboard} />
-        </div>
-        </Fragment>
-      </BrowserRouter>
-    </Fragment>
-   )
- }
+        <BrowserRouter>
+          <Fragment>
+            <div>
+              <Dashboard/>
+            </div>
+          </Fragment>
+        </BrowserRouter>
+      </Fragment>
+    );
+  }
 }
